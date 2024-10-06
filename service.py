500f6c1e457f793ce654f7798afcb9bfe72aae32
ChAniKi <alex.0070940@mail.ru>
2024-10-06 22:59:05 +0300

    def - login and password

--- service.py
+++ service.py
@@ -1,2 +1,28 @@
-#service app
+import re
 
+def check_login(login):
+    if re.fullmatch(r'[A-Za-z]+', login):
+        return True
+    return False
+
+def check_password(password):
+    if (re.search(r'[a-z]', password) and
+        re.search(r'[A-Z]', password) and
+        re.search(r'\d', password) and
+        re.fullmatch(r'[A-Za-z0-9]+', password)):
+        return True
+    return False
+
+
+login = "UserLogin"
+password = "Password123"
+
+if check_login(login):
+    print("Логин корректен")
+else:
+    print("Логин некорректен")
+
+if check_password(password):
+    print("Пароль корректен")
+else:
+    print("Пароль некорректен")
