c167115d2e48049cc9aecff2b877680176895c61
ChAniKi <alex.0070940@mail.ru>
2024-10-06 23:52:17 +0300

    New_Def

--- service.py
+++ service.py
@@ -1,28 +1,20 @@
-import re
-
 def check_login(login):
-    if re.fullmatch(r'[A-Za-z0-9]+', login):
-        return True
-    return False
+    return len(login) >= 6 and all('A' <= char <= 'Z' or 'a' <= char <= 'z' or '0' <= char <= '9' for char in login)
 
 def check_password(password):
-    if (re.search(r'[a-z]', password) and
-        re.search(r'[A-Z]', password) and
-        re.search(r'\d', password) and
-        re.fullmatch(r'[A-Za-z0-9]+', password)):
-        return True
-    return False
+    if len(password) < 6:
+        return False
 
+    has_lower = has_upper = has_digit = False
 
-login = input()
-password = input()
+    for char in password:
+        if 'a' <= char <= 'z':
+            has_lower = True
+        elif 'A' <= char <= 'Z':
+            has_upper = True
+        elif '0' <= char <= '9':
+            has_digit = True
+        elif 'А' <= char <= 'Я' or 'а' <= char <= 'я':
+            return False
 
-if check_login(login):
-    print("Логин корректен")
-else:
-    print("Логин некорректен")
-
-if check_password(password):
-    print("Пароль корректен")
-else:
-    print("Пароль некорректен")
+    return has_lower and has_upper and has_digit
