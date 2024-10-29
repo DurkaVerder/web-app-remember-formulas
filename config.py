c446d272e11218284cd2c6aeed704f2cfbfca207
tlyashok <tlyashok20200@gmail.com>
2024-10-29 17:09:04 +0300

    deploy

--- config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///web-site-for-remember-formulas.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'secret_key'
-    CHIPHER_KEY = 3
