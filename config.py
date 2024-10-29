b5679cf8707c97c2bf053b7757b56f203098f76e
tlyashok <tlyashok20200@gmail.com>
2024-10-29 11:20:05 +0300

    deploy

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:durka@localhost/web-site-for-remember-formulas'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///web-site-for-remember-formulas.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'secret_key'
     CHIPHER_KEY = 3
