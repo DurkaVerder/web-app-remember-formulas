195452f185c5a42bf81ebcca2cfc7c2e47a77dae
DurlaVerder <vrrrr227@gmail.com>
2025-05-03 17:01:11 +0300

    Fix path

--- src/config.py
+++ src/config.py
@@ -1,5 +1,10 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db_sqlite.db'
+    BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    DB_PATH = os.path.join(BASE_DIR, 'instance', 'db_sqlite.db')
+
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{DB_PATH}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'secret_key'
     CHIPHER_KEY = 3
